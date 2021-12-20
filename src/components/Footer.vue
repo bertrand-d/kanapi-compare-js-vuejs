f5e27746e7510dc8c69fc379c0dad856bc6991c8
reuby <delphineb57@gmail.com>
2021-12-20 11:48:17 +0100

    split footer and header in own component

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+<footer>
+    <div class="limitedWidthBlockContainer footerMain">
+      <div class="limitedWidthBlock">
+        <div>
+          <img class="logo" src="../images/logo.png" alt="Logo de l'entreprise">
+        </div>
+        <div>
+          <p>10 quai de la charente <br>75019 Paris 19</p>
+        </div>
+        <div>
+          <p>Téléphone : 01 23 45 67 89</p>
+        </div>
+        <div>
+          <p>Email : support@name.com</p>
+        </div>
+      </div>
+    </div>
+    <div class="limitedWidthBlockContainer footerSecondary">
+      <div class="limitedWidthBlock">
+        <p>© Copyright 2021 - 2042 | Openclassrooms by Openclassrooms | All Rights Reserved | Powered by 3</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+</script>
+
+<style lang ="scss">
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+        <div class="limitedWidthBlockContainer informations">
+          <div class="limitedWidthBlock">
+              <ul>
+                <li><img src="./images/icons/phone.svg" alt="logo de téléphone" class="informations__phone">01 23 45 67 89
+                </li>
+                <li><img src="./images/icons/mail.svg" alt="logo d'une enveloppe" class="informations__mail">support@name.com
+                </li>
+                <li><img src="./images/icons/adress.svg" alt="logo d'un point de géolocalisation"
+                    class="informations__address">01 23 45 67 89</li>
+              </ul>
+          </div>
+        </div>
+        <div class="limitedWidthBlockContainer menu">
+        <div class="limitedWidthBlock">
+            <a href="index.html">
+            <img class="logo" src="./images/logo.png" alt="Logo de l'entreprise">
+            </a>
+            <nav>
+            <ul>
+                <a href="index.html">
+                <li>Accueil</li>
+                </a>
+                <a href="cart.html">
+                <li>Panier</li>
+                </a>
+            </ul>
+            </nav>
+        </div>
+        </div>
+        <img class="banniere" src="./images/banniere.png" alt="Baniere">
+    </header>
+</template>
+
+<script>
+</script>
+
+<style lang ="scss">
+</style>
