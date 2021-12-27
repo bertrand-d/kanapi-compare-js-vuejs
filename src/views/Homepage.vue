11ed8fd9b4f184c2057ee8fdf7466a1c0aaa5278
reuby <delphineb57@gmail.com>
2021-12-27 16:06:36 +0100

    fix link cart in header

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -3,15 +3,18 @@
         <main class="limitedWidthBlockContainer">
             <div class="limitedWidthBlock">
                 <div class="titles">
-                    <h1>Nos produits</h1>
-                    <h2>Une gamme d'articles exclusifs</h2>
+                    <h1 v-if="apiError">Une erreur est survenue, merci de revenir plus tard</h1>
+                    <template v-else>
+                        <h1>Nos produits</h1>
+                        <h2>Une gamme d'articles exclusifs</h2>
+                    </template>
                 </div>
                 <section class="items" id="items">
                     <router-link :to="`/product?id=${sofa._id}`" exact v-for="sofa in sofaList" :key=sofa.id>
                         <article >
                             <img :src="sofa.imageUrl" :alt="sofa.altTxt">
-                            <h3 class="productName">{{ sofa.name }}</h3>
-                            <p class="productDescription">{{ sofa.description }}</p>
+                            <h3>{{ sofa.name }}</h3>
+                            <p>{{ sofa.description }}</p>
                         </article>
                     </router-link>
                 </section>
@@ -21,14 +24,14 @@
 </template>
 
 <script>
-
 import axios from 'axios'
 
 export default {
   name: 'Homepage',
   data(){
       return {
-          sofaList : []
+          sofaList : [],
+          apiError : false
       }
   },
   mounted(){
@@ -38,10 +41,13 @@ export default {
           this.sofaList = response.data;
           console.log(this.sofaList)
       })
+      .catch(error => {
+          this.apiError = true;
+          console.log(error)
+      })
   },
 }
 </script>
 
 <style lang="scss">
-
 </style>
